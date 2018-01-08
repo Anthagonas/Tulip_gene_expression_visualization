a160b1dd04e5d625de5134f88d4eab78b3b4f514
Anthagonas <the_rod_21@hotmail.com>
2018-01-08 23:14:58 +0100

    started preprocessing script
    Startint part 1 of the poject
    TODO : change anchro shape of the edges and set nodes positions

---
+++ analyse_visu_expr_gen.py
@@ -0,0 +1,104 @@
+# Powered by Python 3.6
+
+# To cancel the modifications performed by the script
+# on the current graph, click on the undo button.
+
+# Some useful keyboards shortcuts : 
+#   * Ctrl + D : comment selected lines.
+#   * Ctrl + Shift + D  : uncomment selected lines.
+#   * Ctrl + I : indent selected lines.
+#   * Ctrl + Shift + I  : unindent selected lines.
+#   * Ctrl + Return  : run script.
+#   * Ctrl + F  : find selected text.
+#   * Ctrl + R  : replace selected text.
+#   * Ctrl + Space  : show auto-completion dialog.
+
+from tulip import tlp
+
+# The updateVisualization(centerViews = True) function can be called
+# during script execution to update the opened views
+
+# The pauseScript() function can be called to pause the script execution.
+# To resume the script execution, you will have to click on the "Run script " button.
+
+# The runGraphScript(scriptFile, graph) function can be called to launch
+# another edited script on a tlp.Graph object.
+# The scriptFile parameter defines the script name to call (in the form [a-zA-Z0-9_]+.py)
+
+# The main(graph) function must be defined 
+# to run the script on the current graph
+
+def EdgesNodesColors(graph, n, pos, neg, color, shape, size, tgtShape, tgtSize):
+  color[n] = tlp.Color.Azure
+  shape[n] = tlp.NodeShape.Sphere
+  for e in graph.getOutEdges(n):
+    if pos[e]:
+      if neg[e]: #regulation positive et négative
+        color[e] = tlp.Color.MagentaRose
+        tgtShape[e] = tlp.EdgeExtremityShape.Diamond
+        tgtSize[e] = size
+      else: #regulation positive sur le node n
+        color[e] = tlp.Color.Green
+        tgtShape[e] = tlp.EdgeExtremityShape.Arrow
+        tgtSize[e] = size
+    else:
+      if neg[e]: #regulation negative sur le node n
+        color[e] = tlp.Color.Red
+        tgtShape[e] = tlp.EdgeExtremityShape.Cross
+        tgtSize[e] = size
+      else: #aucune regulation
+        color[e] = tlp.Color.Black
+  
+
+def main(graph): 
+  Locus = graph.getStringProperty("Locus")
+  Negative = graph.getBooleanProperty("Negative")
+  Positive = graph.getBooleanProperty("Positive")
+  tp1_s = graph.getDoubleProperty("tp1 s")
+  tp10_s = graph.getDoubleProperty("tp10 s")
+  tp11_s = graph.getDoubleProperty("tp11 s")
+  tp12_s = graph.getDoubleProperty("tp12 s")
+  tp13_s = graph.getDoubleProperty("tp13 s")
+  tp14_s = graph.getDoubleProperty("tp14 s")
+  tp15_s = graph.getDoubleProperty("tp15 s")
+  tp16_s = graph.getDoubleProperty("tp16 s")
+  tp17_s = graph.getDoubleProperty("tp17 s")
+  tp2_s = graph.getDoubleProperty("tp2 s")
+  tp3_s = graph.getDoubleProperty("tp3 s")
+  tp4_s = graph.getDoubleProperty("tp4 s")
+  tp5_s = graph.getDoubleProperty("tp5 s")
+  tp6_s = graph.getDoubleProperty("tp6 s")
+  tp7_s = graph.getDoubleProperty("tp7 s")
+  tp8_s = graph.getDoubleProperty("tp8 s")
+  tp9_s = graph.getDoubleProperty("tp9 s")
+  viewBorderColor = graph.getColorProperty("viewBorderColor")
+  viewBorderWidth = graph.getDoubleProperty("viewBorderWidth")
+  viewColor = graph.getColorProperty("viewColor")
+  viewFont = graph.getStringProperty("viewFont")
+  viewFontSize = graph.getIntegerProperty("viewFontSize")
+  viewIcon = graph.getStringProperty("viewIcon")
+  viewLabel = graph.getStringProperty("viewLabel")
+  viewLabelBorderColor = graph.getColorProperty("viewLabelBorderColor")
+  viewLabelBorderWidth = graph.getDoubleProperty("viewLabelBorderWidth")
+  viewLabelColor = graph.getColorProperty("viewLabelColor")
+  viewLabelPosition = graph.getIntegerProperty("viewLabelPosition")
+  viewLayout = graph.getLayoutProperty("viewLayout")
+  viewMetric = graph.getDoubleProperty("viewMetric")
+  viewRotation = graph.getDoubleProperty("viewRotation")
+  viewSelection = graph.getBooleanProperty("viewSelection")
+  viewShape = graph.getIntegerProperty("viewShape")
+  viewSize = graph.getSizeProperty("viewSize")
+  viewSrcAnchorShape = graph.getIntegerProperty("viewSrcAnchorShape")
+  viewSrcAnchorSize = graph.getSizeProperty("viewSrcAnchorSize")
+  viewTexture = graph.getStringProperty("viewTexture")
+  viewTgtAnchorShape = graph.getIntegerProperty("viewTgtAnchorShape")
+  viewTgtAnchorSize = graph.getSizeProperty("viewTgtAnchorSize")
+  baseSize= tlp.Size(100.0,100.0,100.0)
+  updateVisualization(centerViews = True)
+
+
+  for n in graph.getNodes():
+    viewLabel[n] = Locus[n]
+    viewSize[n] = baseSize * (graph.deg(n)+1)
+    EdgesNodesColors(graph, n, Positive, Negative, viewColor, 
+    viewShape, viewSize[n]/2 , viewTgtAnchorShape, viewTgtAnchorSize)
